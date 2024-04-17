7a179ca3220005c734e2ea4894fe6ade8d30b5c6
Nashmi Shah <nashmi.shah@seamlessx.com>
2024-04-17 20:47:18 +0800

    amended base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/psd-directus/',
+  base: '/psd-frontend/',
   plugins: [react()],
 })
 
